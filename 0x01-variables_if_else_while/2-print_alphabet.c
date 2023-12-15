4d16aef824671e30607986ec5dec0dba8f9313b0
khlood-ebada <khloodebada97@gmail.com>
2023-12-15 05:33:43 -0800

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - entery point
+ *
+ * Description: print all alphabe letters
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch = a;
+
+	while (ch <= z)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar(`\n`);
+	return (0);
+}
