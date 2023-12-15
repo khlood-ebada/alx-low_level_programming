9f06e3ff2e61df440d29f7d933000beea8ef94d4
khlood-ebada <khloodebada97@gmail.com>
2023-12-15 11:36:12 -0800

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - Entery point
+ * Description: print a line
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("Programming is like building a multilingual puzzle"\n);
