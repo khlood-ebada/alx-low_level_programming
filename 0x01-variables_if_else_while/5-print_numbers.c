e10928091f723caa5df2e8c5d4777e3f25445229
khlood-ebada <khloodebada97@gmail.com>
2023-12-15 07:35:23 -0800

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * Descrption: prints all single digit numbers of base 10 starting from 0
+ * Description: prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
 
