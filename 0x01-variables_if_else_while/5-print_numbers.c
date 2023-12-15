d2102821b04fcb70537d10660e1a90c5b6d2211f
khlood-ebada <khloodebada97@gmail.com>
2023-12-15 10:44:55 -0800

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * Description: prints all single digit numbers of base 10 starting from 0
+ * Description: prints all single digit numbers
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
 	int digit = 0;
 
-	while digit <= 9)
+	while (digit <= 9)
 	{
 		printf("digit = %d\n", digit);
 		digit++;
