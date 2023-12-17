c20d5f36b3769eb34d32574c5aa589651fb6d772
khlood-ebada <khloodebada97@gmail.com>
2023-12-17 11:20:15 -0800

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include<unistd.h>
 /**
  * main - Entery point
- * Description: print a line to standard error
  * Return: 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
 	return (1);
 }
