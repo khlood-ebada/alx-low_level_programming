bc64100b9ba08aa04bad6cdcfa8eeb88d355731c
khlood-ebada <khloodebada97@gmail.com>
2023-12-18 04:07:27 -0800

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
