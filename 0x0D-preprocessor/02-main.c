b12e05424784b13f0032fec9ec08655c727639d0
khlood-ebada <khloodebada97@gmail.com>
2024-01-22 02:48:13 -0800

    a

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/*
- * main - prints the name of the file
+/**
+ * main - print the file name
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
+ *
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
+
 	return (0);
 }
