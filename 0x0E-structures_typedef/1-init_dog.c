9a5326b2e502c13cf4b4b329c46abd93b6853413
khlood-ebada <khloodebada97@gmail.com>
2024-01-23 11:01:42 -0800

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
-#ifndef DOG_H
-#define DOG_H
+#include <dog.h>
 
 /**
  *struct dog - a dog struct
  *@name: dog name
  *@age: dog age
  *@owner: dog owner
+ *Return: void
  */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif
+{
+	if (d)
+	{
+	d->name = name;
+	d->age = age;
+	d->onwer = owner;
+	}
+
+}
