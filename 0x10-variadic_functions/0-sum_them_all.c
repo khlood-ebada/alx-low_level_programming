125c211f6f758fd5ca6b7d344bfee7ac14d3c15e
khlood-ebada <khloodebada97@gmail.com>
2024-01-25 06:39:46 -0800

    a

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of variables
+ * @n: no of arguments
+ * @...: intgers
+ * Return: the integer sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i = n;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
