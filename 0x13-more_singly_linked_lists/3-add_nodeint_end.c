46c35d9a1b9092f3bf4f613cd70e084a8bea0a7e
khlood-ebada <khloodebada97@gmail.com>
2024-02-06 05:45:38 -0800

    a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,9 +16,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	new_node->next = NULL;
 	new_node->n = n;
-	if (!*head)
-		*head = new_node;
-	else
+	if (*head == NULL)
+		*head = new_node; else
 	{
 		node = *head;
 		while (node->next)
