c1f861c6f48b707272a6d14381a8afb108483056
khlood-ebada <khloodebada97@gmail.com>
2024-02-06 04:45:44 -0800

    a

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!head || !new_node)
 		return (NULL);
 
-	new_node->next = NULL
+	new_node->next = NULL;
 	new_node->n = n;
 	if (*head)
 		new_node->next = *head;
