d7d11c93caafa09664db746e053a55586a5c9c83
khlood-ebada <khloodebada97@gmail.com>
2024-02-13 11:54:27 -0800

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,5 +21,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-	return (0);
+	return (bytes);
 }
