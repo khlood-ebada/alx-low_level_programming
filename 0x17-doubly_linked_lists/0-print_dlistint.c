66353d0fb69c44ddeb8535a9f6bda44d93d714b7
khlood-ebada <khloodebada97@gmail.com>
2024-03-22 14:32:37 -0700

    a

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		i++
+		count++;
 		h = h->next;
 	}
-	return (i);
+	return (count);
 }
