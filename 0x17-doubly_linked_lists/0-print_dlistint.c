90de09fd403fb151c7acd22fd97a59efedf1af18
khlood-ebada <khloodebada97@gmail.com>
2024-03-22 14:17:29 -0700

    a

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints doubly linked list
+ * @h: pointer pointing to the first node
+ * Return: no of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		i++
+	}
+	return (i);
+}
